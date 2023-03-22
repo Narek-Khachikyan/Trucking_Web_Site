5bf0b22856d0b9543658fd0c06087b018b4f91b9
Narek Khachikyan <112387347+magiccwarss@users.noreply.github.com>
2023-03-22 19:11:22 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navigation_and_other");
+const bodyes = document.querySelector("body")
+
+hamburger.addEventListener("click",() =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    bodyes.classList.toggle("lock");
+
+})
